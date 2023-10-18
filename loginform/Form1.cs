cafe40b60b1e4c6cb72369aa319b9a0b682f1c3b
deepak sabu <deepaksabu52@gmail.com>
2023-10-18 13:08:42 +0530

    Added error messasge for password validation

--- loginform/Form1.cs
+++ loginform/Form1.cs
@@ -1,5 +1,7 @@
 using System.Data;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
 namespace loginform
 {
     public partial class Form1 : Form
@@ -16,46 +18,61 @@ namespace loginform
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "")
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
             {
-                MessageBox.Show("Username Should not be empty");
-
+                MessageBox.Show("Username should not be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else if (textBox2.Text == "")
+            else if (string.IsNullOrWhiteSpace(textBox2.Text))
             {
-                MessageBox.Show("Password should not be empty ");
+                MessageBox.Show("Password should not be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
-                try
+                string password = textBox2.Text;
+
+                // Password validation rules (you can customize these rules):
+                if (password.Length < 8)
                 {
-                    SqlConnection conn = new SqlConnection("Data Source=LAPTOP-8MDS7I11\\SQLEXPRESS;Initial Catalog=WindowsForms;Integrated Security=True");
-                    SqlCommand cmd = new SqlCommand("SELECT * FROM Login WHERE username = @username AND password = @password", conn);
-                    cmd.Parameters.AddWithValue("@username", textBox1.Text);
-                    cmd.Parameters.AddWithValue("@Password", textBox2.Text);
-                    SqlDataAdapter da = new SqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-
-
-
-                    da.Fill(dt);
-
-                    if (dt.Rows.Count > 0)
-                    {
-                        MessageBox.Show("Login Sucess");
-                    }
-                    else
-                    {
-                        MessageBox.Show("User Name or Password Invalid!");
-                    }
+                    MessageBox.Show("Password must be at least 8 characters long.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-                catch (Exception ex)
+                else if (!Regex.IsMatch(password, @"[A-Z]"))
                 {
-                    MessageBox.Show("" + ex);
+                    MessageBox.Show("Password must contain at least one uppercase letter.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-                finally
+                else if (!Regex.IsMatch(password, @"[a-z]"))
                 {
+                    MessageBox.Show("Password must contain at least one lowercase letter.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (!Regex.IsMatch(password, @"[0-9]"))
+                {
+                    MessageBox.Show("Password must contain at least one digit.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    try
+                    {
+                        SqlConnection conn = new SqlConnection("Data Source=LAPTOP-8MDS7I11\\SQLEXPRESS;Initial Catalog=WindowsForms;Integrated Security=True");
+                        SqlCommand cmd = new SqlCommand("SELECT * FROM Login WHERE username = @username AND password = @password", conn);
+                        cmd.Parameters.AddWithValue("@username", textBox1.Text);
+                        cmd.Parameters.AddWithValue("@password", textBox2.Text);
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
 
+                        da.Fill(dt);
+
+                        if (dt.Rows.Count > 0)
+                        {
+                            MessageBox.Show("Login Success");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Username or Password Invalid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                 }
             }
         }
